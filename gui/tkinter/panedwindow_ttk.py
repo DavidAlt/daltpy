ba6e84e299d62eed9cfcbdf254c6a61d7561e547
David Alt <david.alt.mil@gmail.com>
2018-12-10 08:06:20 -0800

    cleanup, renamed

--- gui/tkinter/panedwindow_ttk.py
+++ gui/tkinter/panedwindow_ttk.py
@@ -5,21 +5,13 @@ from tkinter import ttk
 
 # setup the panedwindow
 root = tk.Tk()
-paned_w = ttk.Panedwindow(root, orient=tk.HORIZONTAL, width=300, height=300)
+paned_w = ttk.PanedWindow(root, orient=tk.HORIZONTAL, width=300, height=300)
 paned_w.pack(fill='both', expand=True)
 
 # setup the panels
 left_p = ttk.Label(paned_w, background='red')
 right_p = ttk.Label(paned_w, background='yellow')
-paned_w.add(left_p, weight=1, minsize=10)
+paned_w.add(left_p, weight=1)
 paned_w.add(right_p, weight=3)
-
-#paned_w.paneconfig(left_p, minsize=10)
-#paned_w.paneconfig()
-
-
-#paned_w.configure(width=600)
-#print(paned_w.pane(left_p))
-#print(paned_w.paneconfigure())
 
 root.mainloop()
